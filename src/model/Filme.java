2f9c2366933cfc361c126f94f2bcf0c8d61c464e
Plácido da Silva França <lifetragicomico@gmail.com>
2026-05-08 08:33:58 -0300

    feat: implementação inicial da classe Filme.

--- src/model/Filme.java
+++ src/model/Filme.java
@@ -1,5 +1,70 @@
 package model;
 
 public class Filme {
+    private Integer id;
+    private String nome;
+    private Integer ano;
+    private String sinopse;
+    private String genero;
 
+    public Filme() {    
+    }
+
+    public Filme(Integer id, String nome, Integer ano, String sinopse, String genero) {
+        setId(id);
+        setNome(nome);
+        setAno(ano);
+        setSinopse(sinopse);
+        setGenero(genero);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setId(Integer id) {
+        if (id != null && id > 0) {
+            this.id = id;
+        }
+    }
+
+    public void setNome(String nome) {
+        if (nome != null) {
+            this.nome = nome;
+        }
+    }
+
+    public void setAno(Integer ano) {
+        if (ano != null && ano > 0) {
+            this.ano = ano;
+        }
+    }
+
+    public void setSinopse(String sinopse) {
+        if (sinopse != null) {
+            this.sinopse = sinopse;
+        }
+    }
+
+    public void setGenero(String genero) {
+        if (genero != null) {
+            this.genero = genero;
+        }
+    }
 }
