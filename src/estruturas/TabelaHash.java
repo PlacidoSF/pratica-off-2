f967a4d67058356e59df6071f97c55a12a217f2c
Plácido da Silva França <lifetragicomico@gmail.com>
2026-05-09 11:37:56 -0300

    feat: implementação da minha tabela hash.

---
+++ src/estruturas/TabelaHash.java
@@ -0,0 +1,64 @@
+package estruturas;
+
+import modelo.Filme;
+
+public class TabelaHash {
+    private static final int tamanhoT = 1021;
+    private NoHash[] tabela = new NoHash[tamanhoT];
+    private int comparacoesBusca;
+
+    public TabelaHash() {}
+
+    public int getComparacoesBusca() {
+        return comparacoesBusca;
+    }
+
+    public void setComparacoesBusca(int comparacoesBusca) {
+        this.comparacoesBusca = comparacoesBusca;
+    }
+
+    public int hash(int chave) {
+        return (chave & 0x7FFFFFFF) % tamanhoT;
+    }
+
+    //final da lista
+    public void inserir(int chave, Filme valorFile) {
+        int posicao = hash(chave);
+
+        NoHash noAtual = tabela[posicao];
+        NoHash noAnterior = null;
+
+        if (noAtual == null) {
+            tabela[posicao] = new NoHash(chave, valorFile);
+        } 
+
+        else {
+            while (noAtual != null) {
+                if (noAtual.getChave() == chave) {
+                    break;
+                }
+                noAnterior = noAtual;
+                noAtual = noAtual.getProxNo();
+            }
+
+            if (noAtual == null) {
+                noAnterior.setProxNo(new NoHash(chave, valorFile));
+            } 
+        }
+    }
+
+    public NoHash buscar(int chave) {
+        this.comparacoesBusca = 0;
+        int posicao = hash(chave);
+        NoHash noAtual = tabela[posicao];
+
+        while (noAtual != null) {
+            this.comparacoesBusca++;
+            if (noAtual.getChave() == chave) {
+                return noAtual;
+            }
+            noAtual = noAtual.getProxNo();
+        }
+        return null;
+    }
+}
